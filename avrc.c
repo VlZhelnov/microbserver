d30d1130800744052106a235c614bb0a339a6194
VlZhelnov <vladimir.jelnov2015@yandex.ru>
2020-05-22 11:56:54 +0300

    Creating Timer1 use interrapts

--- avrc.c
+++ avrc.c
@@ -1,13 +1,17 @@
 #define F_CPU 1000000UL
-//#define UART_BAUD 9600
-//#define UBRR_VALUE F_CPU/UART_BAUD/8-1
+#define UART_BAUD 9600
+#define UBRR_VALUE F_CPU/UART_BAUD/8-1
 #include <avr/io.h>
 #include <util/delay.h>
-//#include <avr/interrupt.h>
+#include <avr/interrupt.h>
 //#include <string.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 
-/*
+
+uint32_t minuta = 0;
+
+unsigned char str_minuta[11];
+
 void uart_init(uint16_t ubrr)
 {
 	UBRRH = (uint8_t) (ubrr>>8);
@@ -25,7 +29,28 @@ uint8_t uart_receive(void)
 {	
 	while(!(UCSRA&(1<<RXC)));
 	return UDR;
-}*/
+}
+
+
+void timer1_init(void)
+{
+	TCCR1B |= (1<<WGM12) | 3;
+	TIMSK |= (1<<OCIE1A);
+	OCR1A = 15625;	
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	minuta += 1;
+	unsigned char *p = str_minuta;
+	while (*p != '\0') *p++ = '\0'; 
+	utoa(minuta, str_minuta,10);
+	p = str_minuta;
+	while (*p != '\0') uart_transmit(*p++);
+	uart_transmit('\n');
+	uart_transmit('\r');
+}
+
 
 void pwm_timer_init(void)
 {
@@ -51,19 +76,10 @@ uint16_t adc_convert(void)
 }
 int main(void)
 {
-	//uart_init(UBRR_VALUE);
-	pwm_timer_init();
-	adc_init();
+	uart_init(UBRR_VALUE);
+	timer1_init();
+	sei();
 	while(1)
 	{
-		if (adc_convert() > 650)
-			OCR2 = 128;
-		else if (adc_convert()>700)
-			OCR2 = 190;
-		else if (adc_convert()>750)
-			OCR2 = 255;
-		else
-			OCR2 =0x00;
-		_delay_ms(100);
 	}
-}
+}	
